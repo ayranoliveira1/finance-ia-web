e06e417ce86278bdb68ec442a1942453665d9f1d
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-20 22:35:03 -0300

    feat: add transactions page

---
+++ app/transactions/page.tsx
@@ -0,0 +1,50 @@
+// import { db } from '../_lib/prisma'
+// import { DataTable } from '../_components/ui/data-table'
+// import { transactionColumns } from './_columns'
+// import AddTransactionButton from '../_components/add-transaction-button'
+// import Header from '../_components/header'
+// import { auth } from '@clerk/nextjs/server'
+// import { redirect } from 'next/navigation'
+// import { ScrollArea } from '../_components/ui/scroll-area'
+// import { canUserAddTransaction } from '../_data/can-user-add-transactions'
+
+// const TransactionsPage = async () => {
+//   const { userId } = await auth()
+//   if (!userId) {
+//     redirect('/login')
+//   }
+
+//   const transactions = await db.transaction.findMany({
+//     where: {
+//       userId,
+//     },
+//     orderBy: {
+//       date: 'desc',
+//     },
+//   })
+
+//   const userCanAddTransaction = await canUserAddTransaction()
+
+//   return (
+//     <>
+//       <Header />
+
+//       <div className="flex flex-col space-y-4 overflow-hidden p-6">
+//         <div className="flex w-full items-center justify-between">
+//           <h1 className="text-2xl font-bold">Transações</h1>
+
+//           <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
+//         </div>
+
+//         <ScrollArea className="h-full">
+//           <DataTable
+//             columns={transactionColumns}
+//             data={JSON.parse(JSON.stringify(transactions))}
+//           />
+//         </ScrollArea>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default TransactionsPage
