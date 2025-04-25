68231b6ea501f7711080b9ff14c9634233bd9f55
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-04-25 19:18:48 -0300

    feat: add acquire plan button component

---
+++ app/admin/subscription/components/acquire-plan-button.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+// import { loadStripe } from '@stripe/stripe-js'
+
+// import { createStripeCheckout } from '../_actions/create-stripe-checkout'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { useAuth } from '@/auth/useAuth'
+import { paymentStripe } from '@/http/payment-stripe'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { LoaderCircleIcon } from 'lucide-react'
+
+const AcquirePlanButton = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const { user, accessToken } = useAuth()
+
+  const router = useRouter()
+
+  const handleAcquirePlanClick = async () => {
+    setIsLoading(true)
+    const response = await paymentStripe(accessToken!)
+    setIsLoading(false)
+
+    const url = response.checkout_url
+
+    router.push(url)
+  }
+
+  const hashPriemiumPlan = user?.subscriptionPlan === 'PREMIUM'
+
+  if (hashPriemiumPlan) {
+    return (
+      <Button
+        asChild
+        className="w-full rounded-full border border-[#55B02E] font-bold"
+        variant="link"
+      >
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.email}`}
+        >
+          Gerenciar Plano
+        </Link>
+      </Button>
+    )
+  }
+
+  return (
+    <Button
+      className="w-full rounded-full bg-[#55B02E] text-white font-bold"
+      onClick={handleAcquirePlanClick}
+    >
+      {isLoading ? (
+        <>
+          <LoaderCircleIcon className="animate-spin" /> carregando
+        </>
+      ) : (
+        'Adquirir Plano'
+      )}
+    </Button>
+  )
+}
+
+export default AcquirePlanButton
