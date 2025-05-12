21b796aa23f3b2323013f760fd6f326a65e255ba
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-05-12 18:53:40 -0300

    feat: add can user add transaction validate

---
+++ lib/can-user-add-transactions.ts
@@ -0,0 +1,31 @@
+import { authOptions } from '@/auth/auth.config'
+import { getCurrentMonthTransactions } from '@/http/get-current-month-transactions'
+import { getServerSession } from 'next-auth'
+
+export const canUserAddTransaction = async () => {
+  const session = await getServerSession(authOptions)
+
+  const accessToken = session?.accessToken
+  if (!accessToken) {
+    throw new Error('Unauthorized')
+  }
+  const user = session?.user
+
+  if (!user) {
+    throw new Error('Unauthorized')
+  }
+
+  const userPlan = user.subscriptionPlan
+
+  if (userPlan === 'PREMIUM') {
+    return true
+  }
+
+  const currentMonthTransactions = await getCurrentMonthTransactions(
+    accessToken!,
+  )
+  if (currentMonthTransactions.count >= 10) {
+    return false
+  }
+  return true
+}
