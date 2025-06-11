de46897fecbf7dd82076091c2a8e5d2aecf0bc08
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-06-11 19:46:00 -0300

    feat: add session type

---
+++ types/session.ts
@@ -0,0 +1,12 @@
+export interface Session {
+  id: string
+  ip: string
+  browser: string
+  os: string
+  deviceType: string
+  country: string
+  city: string
+  region: string
+  createdAt: Date
+  updatedAt: Date
+}
